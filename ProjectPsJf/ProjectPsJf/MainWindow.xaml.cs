5fd0e873f438f751340c09e3d97e4f93b66abac5
psaxin <pjar_Saxin@hotmail.com>
2015-10-08 15:16:41 +0200

    TESTAR IGEN

--- ProjectPsJf/ProjectPsJf/MainWindow.xaml.cs
+++ ProjectPsJf/ProjectPsJf/MainWindow.xaml.cs
@@ -66,7 +66,7 @@ namespace ProjectPsJf
             {
                 MessageBox.Show("URL fungerade ej");
 
-                // denna metod är ännu inte kommenterad.
+             //TOG BORT KOMMENTAREN
                 
             }
         }
